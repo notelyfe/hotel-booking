c543e37cd4f16c2ca895690c812089a5a88691e7
Ankesh Kumar <60851047+NoteLyfe@users.noreply.github.com>
2022-09-11 14:34:23 +0530

    update 1.0

--- src/component/AddConfirm.js
+++ src/component/AddConfirm.js
@@ -8,15 +8,15 @@ const AddConfirm = (addConfirm) => {
     }
 
   return (
-    <div className='d-flex justify-content-around align-items-center rounded bg-success text-light' 
+    <div className='d-flex justify-content-around align-items-center rounded bg-success text-light m-auto' 
     style={{
-        width: "50%",
+        width: "60%",
         height: "65px",
         position: "absolute",
         top: "240px",
         left: "25%"
     }}>
-      <h5>Customer Added Successfully</h5>
+      <h5 className='my-2 mx-2'>Customer Added Successfully</h5>
       <button onClick={close} className='btn text-light' style={{border: "none", fontSize:"20px"}}>&times;</button>
       <a id='addConfirm' hidden href="/">link</a>
     </div>
