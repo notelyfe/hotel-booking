0fbadfdc3b3c9fcfd7a6a9a805acd86cd1ead59f
Ankesh Kumar <60851047+NoteLyfe@users.noreply.github.com>
2022-09-14 19:46:40 +0530

    css-bug-fix

--- src/component/ConfirmMsg.js
+++ src/component/ConfirmMsg.js
@@ -5,7 +5,7 @@ const ConfirmMsg = () => {
   return (
     <div className='container d-flex'>
       <h2 className='text-center' style={{marginTop: "80px"}}> You have Successfully Checked Out... </h2>
-      <Link className='text-center' to="/">Go To Home</Link>
+      <Link className='text-center' to="/" style={{marginTop: "130px"}}>Go To Home</Link>
     </div>
   )
 }
